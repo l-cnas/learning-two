f79e3eb32376b57565f59ab949fdd68d611cc885
l-cnas <lin.narbutas@gmail.com>
2025-11-10 14:12:16 +0200

    Js start

---
+++ 011/app.js
@@ -0,0 +1,11 @@
+console.log ('Labas Bebre!');
+
+
+var x = 10;
+
+console.log(x);
+
+
+var pasisveikimasSuBebru = 'Labas is ryto ponas Bebre!';
+
+console.log(pasisveikimasSuBebru, 'pasisveikinimasSuBebru');
