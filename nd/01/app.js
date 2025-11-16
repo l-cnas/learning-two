1bd1674f5475ea90e20c78110bacc538567aa8eb
l-cnas <lin.narbutas@gmail.com>
2025-11-16 12:41:16 +0200

    homework headache

--- nd/01/app.js
+++ nd/01/app.js
@@ -122,3 +122,93 @@ divas3.style.height = big + 'px';
 divas3.style.width = big + 'px';
 divas3.style.borderRadius = '50%';
 divas3.style.border = "3px solid black";
+
+
+
+// 5.
+
+let n1 = rand(-10, 10);
+let n2 = rand(-10, 10);
+let n3 = rand(-10, 10);
+
+const sp1 = document.querySelector('#go5 span:nth-child(1)');
+const sp2 = document.querySelector('#go5 span:nth-child(2)');
+const sp3 = document.querySelector('#go5 span:nth-child(3)');
+
+sp1.innerText = n1;
+if (n1 < 0) sp1.style.color = 'red';
+else if (n1 > 0) sp1.style.color = 'blue';
+else sp1.style.color = 'green';
+
+sp2.innerText = n2;
+if (n2 < 0) sp2.style.color = 'red';
+else if (n2 > 0) sp2.style.color = 'blue';
+else sp2.style.color = 'green';
+
+sp3.innerText = n3;
+if (n3 < 0) sp3.style.color = 'red';
+else if (n3 > 0) sp3.style.color = 'blue';
+else sp3.style.color = 'green';
+
+
+// 6.
+
+let amount = rand(5, 3000);
+
+let subtotal = amount * 1;
+
+let discount = 0;
+if (subtotal > 2000) {
+    discount = 4;
+} else if (subtotal > 1000) {
+    discount = 3;
+}
+
+let total = subtotal - (subtotal * discount / 100);
+
+// data yra custom selectorius
+const pAmount   = document.querySelector('[data-amount]');
+const pSubtotal = document.querySelector('[data-subtotal]');
+const pDiscount = document.querySelector('[data-discount]');
+const pTotal    = document.querySelector('[data-total]');
+
+const spanAmount   = pAmount.querySelector('span');
+const spanSubtotal = pSubtotal.querySelector('span');
+const spanDiscount = pDiscount.querySelector('span');
+const spanTotal    = pTotal.querySelector('span');
+
+spanAmount.innerText   = amount;
+spanSubtotal.innerText = subtotal.toFixed(2);
+spanDiscount.innerText = discount;
+spanTotal.innerText    = total.toFixed(2);
+
+
+
+// 7.
+
+const shape = document.querySelector('#go7 div');
+
+let r = rand(1, 3);
+
+if (r === 1) {
+    shape.style.width = '100px';
+    shape.style.height = '100px';
+    shape.style.backgroundColor = 'blue';
+    shape.style.borderRadius = '50%';
+}
+else if (r === 2) {
+    shape.style.width = '120px';
+    shape.style.height = '120px';
+    shape.style.backgroundColor = 'red';
+}
+// trikampis 
+else {
+    shape.style.width = '0';
+    shape.style.height = '0';
+    shape.style.borderLeft = '60px solid transparent';
+    shape.style.borderRight = '60px solid transparent';
+    shape.style.borderBottom = '100px solid green';
+}
+
+
+
