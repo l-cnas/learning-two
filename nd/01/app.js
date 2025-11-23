3240fefa19ac5fbc7c44736d00313573cc7cdaf0
l-cnas <lin.narbutas@gmail.com>
2025-11-23 19:53:09 +0200

    nd01 baigiam

--- nd/01/app.js
+++ nd/01/app.js
@@ -167,20 +167,20 @@ if (subtotal > 2000) {
 let total = subtotal - (subtotal * discount / 100);
 
 // data yra custom selectorius
-const pAmount   = document.querySelector('[data-amount]');
+const pAmount = document.querySelector('[data-amount]');
 const pSubtotal = document.querySelector('[data-subtotal]');
 const pDiscount = document.querySelector('[data-discount]');
-const pTotal    = document.querySelector('[data-total]');
+const pTotal = document.querySelector('[data-total]');
 
-const spanAmount   = pAmount.querySelector('span');
+const spanAmount = pAmount.querySelector('span');
 const spanSubtotal = pSubtotal.querySelector('span');
 const spanDiscount = pDiscount.querySelector('span');
-const spanTotal    = pTotal.querySelector('span');
+const spanTotal = pTotal.querySelector('span');
 
-spanAmount.innerText   = amount;
+spanAmount.innerText = amount;
 spanSubtotal.innerText = subtotal.toFixed(2);
 spanDiscount.innerText = discount;
-spanTotal.innerText    = total.toFixed(2);
+spanTotal.innerText = total.toFixed(2);
 
 
 
@@ -209,6 +209,123 @@ else {
     shape.style.borderRight = '60px solid transparent';
     shape.style.borderBottom = '100px solid green';
 }
+
+//8. 
+// Sukuriam kur kist visus gautus duomenis - OP success
+let zali = document.querySelector('#go8 > div[data-result] > p[data-green] > span');
+let raudoni = document.querySelector('#go8 > div[data-result] > p[data-red] > span');
+let melyni = document.querySelector('#go8 > div[data-result] > p[data-blue] > span');
+
+// Testuojam - OP success
+// zali.innerText = 'test';
+// raudoni.innerText = 'test';
+// melyni.innerText = 'test';
+
+// QuerySelectorAll - susikuriam nodelista visko kas yra divuose.
+let colorss = document.querySelectorAll('#go8 > div')
+
+// Nuliai, kintamieji, prie ko bus priskirtos spalvu skaicius.
+let greens = 0;
+let reds = 0;
+let blues = 0;
+
+// Leidziam cikla for. ne velnio be pavyzdzio as jo neprimineku kaip rasyt.
+for (i = 0; i < colorss.length; i++) {
+    if (colorss[i].style.backgroundColor == 'green') {
+        greens += 1;
+    } else {
+        if (colorss[i].style.backgroundColor == 'red') {
+            reds += 1;
+        } else {
+            blues += 1;
+        }
+    }
+}
+
+// Jeronimo!
+zali.innerText = greens;
+raudoni.innerText = reds;
+melyni.innerText = blues;
+
+
+// 9. kai pasigooglini kaip sudet viska i values ir paverst Number, ne taip baisu.
+
+let row1 = document.querySelectorAll('#go9 > div[data-row-1] > span');
+let a1 = Number(row1[0].textContent);
+let b1 = Number(row1[1].textContent);
+row1[2].innerText = a1 * b1;
+
+let row2 = document.querySelectorAll('#go9 > div[data-row-2] > span');
+let a2 = Number(row2[0].textContent);
+let b2 = Number(row2[1].textContent);
+row2[2].innerText = a2 * b2;
+
+let row3 = document.querySelectorAll('#go9 > div[data-row-3] > span');
+let a3 = Number(row3[0].textContent);
+let b3 = Number(row3[1].textContent);
+row3[2].innerText = a3 * b3;
+
+let row4 = document.querySelectorAll('#go9 > div[data-row-4] > span');
+let a4 = Number(row4[0].textContent);
+let b4 = Number(row4[1].textContent);
+row4[2].innerText = a4 * b4;
+
+let row5 = document.querySelectorAll('#go9 > div[data-row-5] > span');
+let a5 = Number(row5[0].textContent);
+let b5 = Number(row5[1].textContent);
+row5[2].innerText = a5 * b5;
+
+// 10. Lets do this! Matematika smirda >.< nu gerai, nesmirda, pas mane tik formules nesifiksuoja :/
+
+let data1 = document.querySelectorAll('#go10 > div[data-sq-1]');
+let data12 = data1[0];
+let width1 = data12.offsetWidth;
+let height1 = data12.offsetHeight;
+let area1 = width1 * height1;
+console.log(width1, height1, area1);
+area1 > 10000 ? data1[0].style.backgroundColor = 'crimson' : data1[0].innerText = 'nespalvinam';
+
+//aight, pavyko, po nemazai googlinimo. terneri pagavom, suzinojom apie duomenu grazinima ir pns. cool.
+//einam dabar toliau, bandom biski susimazint darbo - paoptimizuot, kad maziau rasyt butu. reik gi pirstus taupyt! :D
+
+let data2 = document.querySelectorAll('#go10 > div[data-sq-2]');
+let area2 = data2[0].offsetWidth * data2[0].offsetHeight;
+console.log(area2, 'pratestuojam 2');
+area2 > 10000 && (data2[0].style.backgroundColor = 'crimson'); 
+
+// UU geras, galim && cia panaudot O.o mind blown!.
+
+let data3 = document.querySelectorAll('#go10 > div[data-sq-3]');
+let area3 = data3[0].offsetWidth * data3[0].offsetHeight;
+console.log(area2, 'pratestuojam 3');
+
+if (area3 > 10000) {
+    data3[0].style.backgroundColor = 'crimson';
+}
+
+// Ai cia paprastai pasipraktikuot jauciu pabaigsiu.
+
+let data4 = document.querySelectorAll('#go10 > div[data-sq-4]');
+let area4 = data4[0].offsetWidth * data4[0].offsetHeight;
+console.log(area4, 'patikrinam 4'); // '' svarbus reikalas
+
+if (area4 > 10000) {
+    data4[0].style.backgroundColor = 'crimson';
+} else {
+    data4[0].innerText = 'Mazas kazkoks...'
+}
+
+
+let data5 = document.querySelectorAll('#go10 > div[data-sq-5]');
+let area5 = data5[0].offsetHeight * data5[0].offsetWidth;
+console.log(area5, 'tikrinam 5');
+
+if (area5 > 10000) {
+    data5[0].style.backgroundColor = 'crimson';
+}
+
+
+
 
 
 
