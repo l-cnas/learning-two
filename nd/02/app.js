60e8092fc75bd9a9b5397bcbf064aeb5713f9e78
l-cnas <lin.narbutas@gmail.com>
2025-11-24 21:31:50 +0200

    pradedam nd2

---
+++ nd/02/app.js
@@ -0,0 +1,36 @@
+function rand(min, max) {
+    const minCeiled = Math.ceil(min);
+    const maxFloored = Math.floor(max);
+    return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
+}
+
+
+console.log(rand(0, 100), 'nd 02');
+
+let a1 = rand(1, 100);
+let b1 = rand(1, 100);
+
+const miniFun = function (a, b) {
+    if (a <= b) {
+        return animal = 'Bebras'
+    } else {
+        if (a >= b) {
+            return animal = 'Barsukas'
+        } else {
+            return animal = 'Briedis'
+        }
+
+    }
+};
+
+console.log(miniFun(a1, b1));
+
+
+// 2
+const button1 = document.querySelector('button');
+const btnEvent = document.querySelector('h2');
+
+
+button1.addEventListener('click', function () {
+    btnEvent.innerText = animal;
+});
