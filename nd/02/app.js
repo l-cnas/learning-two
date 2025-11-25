7917578fb2c30459e176ac9e16bfa02a3d73d4ce
l-cnas <lin.narbutas@gmail.com>
2025-11-25 18:03:18 +0200

    nd2

--- nd/02/app.js
+++ nd/02/app.js
@@ -34,3 +34,52 @@ const btnEvent = document.querySelector('h2');
 button1.addEventListener('click', function () {
     btnEvent.innerText = animal;
 });
+
+
+// 3. 
+
+
+const grazinaRaides = function (_) {
+    if (_.length > 3) {
+        return (_[0] + _[1] + _[2]);
+    } else {
+        return _;
+    }
+
+}
+
+console.log(grazinaRaides('bebras'), '-grazin is bebro');
+console.log(grazinaRaides('haha'), '-grazina is haha');
+
+// 4.
+
+const ketvirtas = function (_) {
+    if (_.length % 2 == 0) {
+        return ('Simbolių skaičius lyginis');
+    } else {
+        return _[(Math.floor(_.length / 2))];
+    }
+}
+
+console.log(ketvirtas('zodis'));
+
+// 5.
+
+const calc = function (a, b, c) {
+    if (c == '*' && b == 0 || a == 0) {
+        return 'Is nulio nesidaugina!';
+    } else if (c == '/' && b == 0 || a == 0) {
+        return 'Is nulio nesidalina!';
+    } else if (c == '-') {
+        return (a - b);
+    } else if (c == '+') {
+        return (a + b);
+    } else if (c == '*') {
+        return (a * b);
+    } else if (c == '/') {
+        return (a / b);
+    }
+
+}
+
+console.log(calc(15, 3, '/'));
