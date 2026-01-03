dc9f67b6f039e14e0fe37dbf9ca9290dfe7a9e82
l-cnas <lin.narbutas@gmail.com>
2026-01-03 19:53:11 +0200

    Objektiniai nd 1

--- nd/Objektiniai nd/app.js
+++ nd/Objektiniai nd/app.js
@@ -26,21 +26,35 @@ kibiras.kiekPririnktaAkmenu();
 kibiras.pridetiDaugAkmenu(55);
 kibiras.kiekPririnktaAkmenu();
 
-// 2. 
+// 2. + 6.
+
+console.log(`\n Antras ir sestas uzdaviniai.`)
 
 class pinigine {
     constructor() {
         this.popieriniaiPinigai = 0;
         this.metaliniaiPinigai = 0;
+        this.kiekPopieriniu = 0;
+        this.kiekMetaliniu = 0;
     }
 
     ideti(kiekis) {
         kiekis <= 2 ? this.metaliniaiPinigai += kiekis : this.popieriniaiPinigai += kiekis;
+        kiekis <= 2 ? this.kiekMetaliniu += kiekis : this.kiekPopieriniu += kiekis;
     }
 
     skaiciuoti() {
         console.log(`Is viso pinigu: ${(this.metaliniaiPinigai + this.popieriniaiPinigai)}`)
     }
+
+    monetos() {
+        console.log(`Pinigineje yra ${this.kiekMetaliniu} pinigu.`)
+    }
+
+    banknotai() {
+        console.log(`Pinigineje turime ${this.kiekPopieriniu} banknotu.`)
+    }
+
 }
 
 const rudaPinigine = new pinigine();
@@ -49,6 +63,9 @@ rudaPinigine.ideti(2);
 rudaPinigine.skaiciuoti();
 rudaPinigine.ideti(4);
 rudaPinigine.skaiciuoti();
+rudaPinigine.monetos();
+rudaPinigine.banknotai();
+
 
 // 3. + 4.
 
@@ -64,7 +81,7 @@ class Troleibusas {
         console.log(`Siuo metu troleibuse vaziuoja ${Troleibusas.visiKeleiviai} keleiviai.`);
     }
 
-// 3. apacioje
+    // 3. apacioje
     constructor() {
         this.keleiviuSkaicius = 0;
         this.ghost = 0;
@@ -107,3 +124,52 @@ troleibusas2.islipa(11);
 troleibusas2.vaziuoja();
 
 Troleibusas.keleiviuSkaiciusVisuoseTroleibusuose();
+
+
+// 5.
+
+console.log('\n Penktas uzdavinys')
+
+class PirkiniuKrepselis {
+
+    constructor() {
+        this.turinys = new Map();
+    };
+
+    idetiSureli(kiekis) {
+        const sureliai = this.turinys.get('Surelis') || 0;
+        this.turinys.set('Surelis', sureliai + kiekis);
+    }
+
+    idetiPieno(kiekis) {
+        const pienas = this.turinys.get('Pienas') || 0;
+        this.turinys.set('Pienas', pienas + kiekis);
+    }
+
+    idetiDuonos(kiekis) {
+        const duona = this.turinys.get('Duona') || 0;
+        this.turinys.set('Duona', duona + kiekis);
+    }
+
+    krepselioTurinys() {
+        const tekstas = [...this.turinys]
+            .map(([preke, kiekis]) => `${preke}: ${kiekis}`)
+            .join(', ');
+
+        console.log(`Siuo metu krepselyje turime: ${tekstas}`);
+    }
+
+
+}
+
+const pirkiniuKrepselis = new PirkiniuKrepselis();
+
+pirkiniuKrepselis.idetiDuonos(1);
+pirkiniuKrepselis.idetiDuonos(6);
+pirkiniuKrepselis.idetiPieno(1);
+pirkiniuKrepselis.idetiPieno(1);
+pirkiniuKrepselis.idetiSureli(2);
+pirkiniuKrepselis.idetiSureli(2);
+
+pirkiniuKrepselis.krepselioTurinys();
+
